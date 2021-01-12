ae6f6b842fc75a11675e26c09aa3acf0bb463ed4
Thomas Tang <iam@tangkunyin.com>
2021-01-12 15:48:17 +0800

    update theme
    Signed-off-by: Thomas Tang <iam@tangkunyin.com>

--- themes/hexo-theme-jsimple/gulpfile.js
+++ themes/hexo-theme-jsimple/gulpfile.js
@@ -1,31 +1,46 @@
 'use strict';
 
-const babel = require('gulp-babel');
-const gulp = require('gulp');
-const minifycss = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
+const gulp = require ('gulp');
+const babel = require ('gulp-babel');
+const minifycss = require ('gulp-clean-css');
+const uglify = require ('gulp-uglify');
+const rename = require ('gulp-rename');
+const concat = require ('gulp-concat');
 
-gulp.task('minify-css', async function() {
-    await gulp.src('./source/css/JSimple.css')
-        .pipe(minifycss())
-        .pipe(rename({ extname: '.min.css' }))
-        .pipe(gulp.dest('./source/css'));
+const srcIgnore = [
+  './source/js/lib/zepto.min.js',
+  './source/js/lib/busuanzi.pure.js',
+  './source/js/lib/md5.js',
+];
+gulp.task ('minify-js', async function () {
+  await gulp
+    .src (srcIgnore.concat('./source/js/lib/SimpleCore.js'))
+    .pipe (
+      babel ({
+        ignore: srcIgnore,
+        presets: ['@babel/preset-env']
+      })
+    )
+    .pipe (uglify ())
+    .pipe (concat ('./source/js/SimpleCore.min.js'))
+    .pipe (gulp.dest ('./'));
 });
-gulp.task('minify-js', async function() {
-    await gulp.src('./source/js/SimpleCore.js')
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.src(['./source/js/lib/zepto.min.js', './source/js/lib/busuanzi.pure.mini.js']))
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(gulp.dest('./source/js'));
+gulp.task ('minify-css', async function () {
+  await gulp
+    .src ('./source/css/JSimple.css')
+    .pipe (minifycss ())
+    .pipe (rename ({extname: '.min.css'}))
+    .pipe (gulp.dest ('./source/css'));
 });
 
-process.on('unhandledRejection', error => {
-    console.error('unhandledRejection', error);
-    process.exit(1) // To exit with a 'failure' code
+process.on ('unhandledRejection', error => {
+  console.error ('unhandledRejection', error);
+  process.exit (1); // To exit with a 'failure' code
 });
 
-gulp.task('default', gulp.parallel('minify-css','minify-js', function(done){
-    done();
-}));
+gulp.task (
+  'default',
+  gulp.parallel ('minify-js', 'minify-css', function (done) {
+    done ();
+  })
+);
